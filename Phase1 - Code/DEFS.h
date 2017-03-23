f540c408d0afb02ca5218a790ae50225db363080
ahmibr <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2017-03-23 09:39:14 +0200

    Add enum & change arrangement

--- Phase1 - Code/DEFS.h
+++ Phase1 - Code/DEFS.h
@@ -6,38 +6,59 @@
 //This file contais some global constants and definitions to be used in the project.
 enum ActionType //The actions supported (you can add more if needed)
 {
-	DRAW_LINE,		//Draw Line
-	DRAW_RECT,		//Draw Rectangle
-	DRAW_TRI,		//Draw Triangle
+	DRAW_RECT,	//Draw Rectangle
 	DRAW_CIRC,		//Draw Circle
-	CHNG_DRAW_CLR,	//Change the drawing color
-	CHNG_FILL_CLR,	//Change the filling color
-	CHNG_BK_CLR,	//Change background color
+	DRAW_TRI,		//Draw Triangle
+	DRAW_LINE,		//Draw Line	
+	CHNG_CLR,		//change color
 	ZOOM,           //To zoom
-	CUT,
-	COPY,
-	PASTE,
-	UNDO,
-	DEL,			//Delete a figure(s)
-	MOVE,			//Move a figure(s)
 	RESIZE,			//Resize a figure(s)
-	ROTATE,			//Rotate a figure(s)
-	SEND_BACK,		//Send a figure to the back of all figures
-	BRNG_FRNT,		//Bring a figure to the front of all figures
 	SAVE,			//Save the whole graph to a file
 	LOAD,			//Load a graph from a file
+	CUT,            //To cut
+	COPY,           //To copy
+	PASTE,          //To Paste 
+	DEL,			//Delete a figure(s)
+	UNDO,           //Undo action
+	REDO,           //Redo action
+	SEND_BACK,		//Send a figure to the back of all figures
+	BRNG_FRNT,		//Bring a figure to the front of all figures
 	EXIT,			//Exit the application
-	CHNG_CLR,       //Choose the color palette
+
+	//******************************************************************************************
 	DRAWING_AREA,	//A click on the drawing area
 	STATUS,			//A click on the status bar
-	EMPTY,			//A click on empty place in the toolbar
-
 	TO_DRAW,		//Switch interface to Draw mode
-	TO_PLAY			//Switch interface to Play mode
-
+	TO_PLAY,		//Switch interface to Play mode
+	EMPTY,			//A click on empty place in the toolbar
+	MOVE,			//Move a figure(s)
+	ROTATE			//Rotate a figure(s)
 	///TODO: Add more action types (if needed)
 };
 
+enum ChangeColor {
+	CHNG_DRAW_CLR,	//Change the drawing color
+	CHNG_FILL_CLR,	//Change the filling color
+	CHNG_BK_CLR,	//Change background color
+};
+
+enum Zoom {
+	ZOOM_IN,       //To zoomIn
+	ZOOM_OUT       //T zoomOut
+};
+
+enum Resize {
+	RESIZE_QUARTER,
+	RESIZE_HALF,
+	RESIZE_DOUBLE,
+	RESIZE_FOUR
+};
+
+enum Rotate {
+	NINTY,
+	EIGHTY,
+	SEVENTY
+};
 struct Point	//To be used for figures points
 { int x,y; };
 
