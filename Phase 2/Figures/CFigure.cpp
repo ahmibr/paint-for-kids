efd6ec0256fd342ae235c7e38f540fb17234259a
ahmibr <ahmed.abdelnaby98@eng-st.cu.edu.eg>
2017-04-12 11:15:05 +0200

    Phase 2

---
+++ Phase 2/Figures/CFigure.cpp
@@ -0,0 +1,23 @@
+#include "CFigure.h"
+
+CFigure::CFigure(GfxInfo FigureGfxInfo)
+{ 
+	FigGfxInfo = FigureGfxInfo;	//Default status is non-filled.
+	Selected = false;
+}
+
+void CFigure::SetSelected(bool s)
+{	Selected = s; }
+
+bool CFigure::IsSelected() const
+{	return Selected; }
+
+void CFigure::ChngDrawClr(sf::Color Dclr)
+{	FigGfxInfo.DrawClr = Dclr; }
+
+void CFigure::ChngFillClr(sf::Color Fclr)
+{	
+	FigGfxInfo.isFilled = true;
+	FigGfxInfo.FillClr = Fclr; 
+}
+
---
+++ Phase 2/GUI/Output.h
@@ -0,0 +1,60 @@
+#pragma once
+#ifndef OUPTUT_H
+#define OUPTUT_H
+#include "Input.h"
+#include <vector>
+class Output	//The application manager should have a pointer to this class
+{
+private:
+
+	//private update window for sfml
+	void UpdateWindow() const;
+
+	//Added by us ------- CHOSEN AS POINTERS BECAUSE THEY ARE USED IN CONST FUNCTIONS
+	sf::RenderWindow* pWind;	//Pointer to the Graphics Window
+	sf::Text *statusMessage;		//message text
+	sf::Font messageFont;
+
+
+	sf::RectangleShape *toolbarLine;
+	sf::RectangleShape *windowBackGround;			//background rectangle
+	sf::RectangleShape *stbar;						//statusbar rectangle
+	sf::Texture MenuItemImages[DRAW_ITM_COUNT];		//Menu items images
+	sf::Sprite MenuItemSprites[DRAW_ITM_COUNT];		//Menu items sprites
+	sf::Texture MenuPlayImages[PLAY_ITM_COUNT];		//Play items images
+	sf::Sprite MenuPlaySprites[PLAY_ITM_COUNT];		//Play items sprites
+
+	vector<sf::Drawable*> *drawnObjects;	//vector to redraw shapes any new events
+
+	//////////////////////////////////////////////////////////////////////////////////
+
+public:
+	Output();
+
+	sf::RenderWindow* CreateWind(int, int, int, int) const; //creates the application window
+	void CreateDrawToolBar() const;	//creates Draw mode toolbar & menu
+	void CreatePlayToolBar() const;	//creates Play mode toolbar & menu
+	void CreateStatusBar() const;	//create the status bar
+
+	Input* CreateInput() const; //creates a pointer to the Input object	
+	void ClearStatusBar() const;	//Clears the status bar
+	void ClearDrawArea() const;	//Clears the drawing area
+
+								// -- Figures Drawing functions
+	void DrawRect(Point P1, Point P2, GfxInfo RectGfxInfo, bool selected = false) const;  //Draw a rectangle
+	void DrawLine(Point P1, Point P2, GfxInfo LineGfxInfo, bool selected) const; //Draw a line
+	void DrawCirc(Point C, Point R, GfxInfo CircGfxInfo, bool selected) const;       //Draw a circle
+	void DrawTriangle(Point P1, Point P2, Point P3, GfxInfo TriangleGfxInfo, bool selected) const; //Draw a triangle
+
+
+	void PrintMessage(string msg) const;	//Print a message on Status bar
+
+	sf::Color getCrntDrawColor() const;	//get current drwawing color
+	sf::Color getCrntFillColor() const;	//get current filling color
+	int getCrntPenWidth() const;		//get current pen width
+
+
+	~Output();
+};
+
+#endif
