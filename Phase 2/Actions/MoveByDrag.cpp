a8ebb8e3b242f7601fd5693eed6d38bd99003c66
bassel97 <bassel.ahmed97@eng-st.cu.edu.eg>
2017-04-18 15:19:59 +0200

    some comments added

--- Phase 2/Actions/MoveByDrag.cpp
+++ Phase 2/Actions/MoveByDrag.cpp
@@ -20,18 +20,18 @@ void MoveByDrag::Execute()
 	Output* pOut = pManager->GetOutput();
 	Input* pIn = pManager->GetInput();
 
-	int xpos, ypos, lastXpos, lastYpos;
+	int xpos, ypos, lastXpos, lastYpos;//the current position of x,y and last pos
 
 	pOut->PrintMessage("Dragging selected objects.");
 
 	pIn->GetPointClicked(xpos, ypos);
-	lastXpos = xpos;
-	lastYpos = ypos;
+	lastXpos = xpos;//during the first frame xpos is the last
+	lastYpos = ypos;//during the first frame ypos is the last
 
 	while (xpos != -1 && ypos != -1) {
 
-		dx = xpos - lastXpos;
-		dy = ypos - lastYpos;
+		dx = xpos - lastXpos;//dx is the change in x from current to last frame
+		dy = ypos - lastYpos;//dy is the change in y from current to last frame
 		lastXpos = xpos;
 		lastYpos = ypos;
 
